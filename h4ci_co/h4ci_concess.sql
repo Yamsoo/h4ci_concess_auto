6c58423f8d11cf4e7e03b609097751f83e3a0e4e
Yamsoo <85763929+Yamsoo@users.noreply.github.com>
2021-08-18 18:42:11 +0200

    Update h4ci_concess.sql

--- h4ci_co/h4ci_concess.sql
+++ h4ci_co/h4ci_concess.sql
@@ -1,18 +1,3 @@
-INSERT INTO `addon_account` (`id`, `name`, `label`, `shared`) VALUES
-(10, 'society_concess', 'Concessionnaire', 1);
-
-INSERT INTO `addon_inventory` (`id`, `name`, `label`, `shared`) VALUES
-(17, 'society_concess', 'Concessionnaire', 1);
-
-INSERT INTO `jobs` (`name`, `label`, `whitelisted`, `SecondaryJob`) VALUES
-('concess', 'Concessionnaire', 1, 0);
-
-INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
-(33, 'concess', 0, 'novice', 'Novice', 100, '', ''),
-(34, 'concess', 1, 'experimente', 'Experimenté', 100, '', ''),
-(35, 'concess', 2, 'boss', 'Patron', 100, '', '');
-
-
 CREATE TABLE `vehicles` (
   `name` varchar(60) NOT NULL,
   `model` varchar(60) NOT NULL,
